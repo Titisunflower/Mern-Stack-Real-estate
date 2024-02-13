fff38087b5f3b2b2eccc499a0ab1a99a874336e5
Titisunflower <henuwz@gmail.com>
2024-02-14 01:26:20 +0200

    update

--- client/src/pages/AddHouse.jsx
+++ client/src/pages/AddHouse.jsx
@@ -157,8 +157,6 @@ export default function CreateHouse() {
             placeholder='Name'
             className='border p-3 rounded-lg'
             id='name'
-            maxLength='62'
-            minLength='10'
             required
             onChange={handleChange}
             value={formData.name}
